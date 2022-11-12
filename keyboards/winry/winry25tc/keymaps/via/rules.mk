894f0d427854c221c003fce362b32914ff21a540
RealCarvedArt <78761379+RealCarvedArt@users.noreply.github.com>
2022-11-12 15:21:49 -0500

    Add files via upload

--- keyboards/winry/winry25tc/keymaps/via/rules.mk
+++ keyboards/winry/winry25tc/keymaps/via/rules.mk
@@ -1,5 +1,3 @@
-VIA_ENABLE = yes
-
 # MCU name
 MCU = atmega32u4
 
@@ -7,24 +5,23 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+#   yes = Enable
+#	no = Disable
+BOOTMAGIC_ENABLE = yes		# Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no		# Mouse keys
+EXTRAKEY_ENABLE = yes		# Audio control and System control
+CONSOLE_ENABLE = no			# Console for debug
+COMMAND_ENABLE = no			# Commands for debug and configuration
+NKRO_ENABLE = no			# Enable N-Key Rollover
+BACKLIGHT_ENABLE = no		# Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes		# Enable keyboard RGB underglow functionality
 
-KEY_LOCK_ENABLE = yes     # Enable KC_LOCK support
+KEY_LOCK_ENABLE = yes		# Enable KC_LOCK support
 
-# For VIA compatibility
+# Caution SLEEP_LED_ENABLE uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = yes		# Sleep LED during USB suspend
+
+# For VIA/Vial compatibility
 VIA_ENABLE = yes
-LTO_ENABLE =yes
-LEADER_ENABLE = no
-FAUXCLICKY_ENABLE = no
-MIDI_ENABLE = no
-BLUETOOTH_ENABLE = no
-TERMINAL_ENABLE = no
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
